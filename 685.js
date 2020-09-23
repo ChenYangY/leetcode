0a804e2ee13cceef626000b432013ad393d5036c
chenyangyang <hn2cyy@163.com>
2020-09-23 16:19:09 +0800

    feat(685): enhancing

--- 685.js
+++ 685.js
@@ -9,18 +9,23 @@ var findRedundantDirectedConnection = function(edges) {
         if(!list[edge[0]]) list[edge[0]] = []
         list[edge[0]].push(edge[1])
         if(nodes[edge[1]] >= 0) nodes[edge[1]] ++;
-        else nodes[edge[1]] = 0;
+        else nodes[edge[1]] = 1;
     })
     let nodeNumber = nodes.length;
+    console.log(nodes)
+    console.log(list)
     for(i=nodeNumber-1;i >= 0; i--) {
         if(nodes[i] === 1) {
+            console.log(isCircle(list, nodes[i], nodes[i]))
             if(isCircle(list, nodes[i], nodes[i])) {
                 return [i, list[i].pop()]
             }
             continue;
         }
         if(nodes[i] > 1) {
+            console.log(i)
             let x = find(list, i)
+            console.log(x)
             return [x, list[x].pop()]
         }
     }
@@ -31,9 +36,11 @@ function find(map, x) {
     let mapLen = map.length
     for(let i = 0; i<mapLen; i++) {
         let next = map[i]
+        console.log(i)
+        console.log(next)
         if(!next || next.length <=0) continue;
         let nextLen = next.length;
-        for(let j=nextLen-1;j<nextLen;i++) {
+        for(let j=nextLen-1;j>=0; j--) {
             if(next[j] === x) return i;
         }
     }
@@ -51,3 +58,7 @@ function isCircle(map, node, origin) {
     }
     return flag;
 }
+
+let result = findRedundantDirectedConnection([[1,2], [1,3], [2,3]])
+
+console.log(result)
